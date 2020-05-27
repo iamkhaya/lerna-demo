d7710b75d83932ae95171b0070887446c805e29e
Khayelihle Tshuma <khayelihle.tshuma@gmail.com>
2020-05-27 19:13:11 +0200

    try again again

--- packages/hello-person/src/index.js
+++ packages/hello-person/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class HelloPerson extends React.Component {
   render () {
-    return <h1>Hello, {this.props.name}</h1>;
+    return <h1>Hello, Hello, {this.props.name}</h1>;
   }
 }
 
